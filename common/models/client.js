ba8decaa9b96ad8c409b7e73e564c2379e4051c3
MisaPintor <misael.62886@hotmail.com>
2018-07-25 13:18:01 -0500

    deny access to all methods by everyone, grant access to methods needed just for owner in Client, hide unused methods in Client API

--- common/models/client.js
+++ common/models/client.js
@@ -2,4 +2,30 @@
 
 module.exports = function(Client) {
 
+    Client.disableRemoteMethodByName("upsert");
+    Client.disableRemoteMethodByName("find");
+    Client.disableRemoteMethodByName("replaceOrCreate");
+    Client.disableRemoteMethodByName("prototype.updateAttributes");
+    Client.disableRemoteMethodByName("exists");
+    Client.disableRemoteMethodByName('prototype.__get__accessTokens');
+    Client.disableRemoteMethodByName('prototype.__create__accessTokens');
+    Client.disableRemoteMethodByName('prototype.__delete__accessTokens');
+    Client.disableRemoteMethodByName('prototype.__findById__accessTokens');
+    Client.disableRemoteMethodByName('prototype.__updateById__accessTokens');
+    Client.disableRemoteMethodByName('prototype.__destroyById__accessTokens');
+    Client.disableRemoteMethodByName('prototype.__count__accessTokens');
+    Client.disableRemoteMethodByName('prototype.__findById__projects');
+    Client.disableRemoteMethodByName('prototype.__updateById__projects');
+    Client.disableRemoteMethodByName('prototype.__destroyById__projects');
+    Client.disableRemoteMethodByName('prototype.__count__projects');
+    Client.disableRemoteMethodByName("prototype.verify");
+    Client.disableRemoteMethodByName("changePassword");
+    Client.disableRemoteMethodByName("createChangeStream");
+    Client.disableRemoteMethodByName("confirm");
+    Client.disableRemoteMethodByName("count");
+    Client.disableRemoteMethodByName("findOne");
+    Client.disableRemoteMethodByName("resetPassword");
+    Client.disableRemoteMethodByName("update");
+    Client.disableRemoteMethodByName("upsertWithWhere"); 
+
 };
--- common/models/project.js
+++ common/models/project.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = function(Project) {
-
+    Project.disableRemoteMethodByName("upsert");
+    Project.disableRemoteMethodByName("find");
+    Project.disableRemoteMethodByName("replaceOrCreate");
+    Project.disableRemoteMethodByName("create");
+    Project.disableRemoteMethodByName("prototype.updateAttributes");
+    Project.disableRemoteMethodByName("exists");    
+    Project.disableRemoteMethodByName('prototype.__get__client');
+    Project.disableRemoteMethodByName('prototype.__get__client__projects');
+    Project.disableRemoteMethodByName('prototype.__create__client__projects');
+    Project.disableRemoteMethodByName('prototype.__delete__client__projects');
 };
