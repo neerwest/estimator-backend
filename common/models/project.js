1feeaa01db6feae47e009caf35c60eab1c02a5fe
MisaPintor <misael.62886@hotmail.com>
2018-07-27 10:47:52 -0500

    adds authentication and hide unused endlines of component, project and team apis

--- common/models/project.js
+++ common/models/project.js
@@ -11,4 +11,17 @@ module.exports = function(Project) {
     Project.disableRemoteMethodByName('prototype.__get__client__projects');
     Project.disableRemoteMethodByName('prototype.__create__client__projects');
     Project.disableRemoteMethodByName('prototype.__delete__client__projects');
+    Project.disableRemoteMethodByName('prototype.__findById__components');
+    Project.disableRemoteMethodByName('prototype.__updateById__components');
+    Project.disableRemoteMethodByName('prototype.__destroyById__components');
+    Project.disableRemoteMethodByName('prototype.__get__components__project');
+    Project.disableRemoteMethodByName('prototype.__count__components__teams');
+    Project.disableRemoteMethodByName('prototype.__count__components');
+    Project.disableRemoteMethodByName('prototype.__get__teams__project');
+    Project.disableRemoteMethodByName('prototype.__count__teams');
+    Project.disableRemoteMethodByName("createChangeStream");
+    Project.disableRemoteMethodByName("count");
+    Project.disableRemoteMethodByName("findOne");
+    Project.disableRemoteMethodByName("update");
+    Project.disableRemoteMethodByName("upsertWithWhere");
 };
