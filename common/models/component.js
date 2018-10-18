4162deec2b7ab8f1b769e4b75006468fc05dc717
MisaPintor <misael.62886@hotmail.com>
2018-10-17 19:18:25 -0500

    remove old models and add new models

--- common/models/component.js
+++ common/models/component.js
@@ -1,21 +1,5 @@
 'use strict';
 
 module.exports = function(Component) {
-    /*
-    //Component.disableRemoteMethodByName("upsert");
-    //Component.disableRemoteMethodByName("find");
-    Component.disableRemoteMethodByName("replaceOrCreate");
-    Component.disableRemoteMethodByName("create");
-    Component.disableRemoteMethodByName("prototype.updateAttributes");
-    Component.disableRemoteMethodByName("exists");
-    Component.disableRemoteMethodByName('prototype.__get__project');
-    Component.disableRemoteMethodByName('prototype.__get__teams__component');
-    Component.disableRemoteMethodByName('prototype.__get__teams__project');
-    Component.disableRemoteMethodByName('prototype.__count__teams');
-    Component.disableRemoteMethodByName("createChangeStream");
-    Component.disableRemoteMethodByName("count");
-    //Component.disableRemoteMethodByName("findOne");
-    //Component.disableRemoteMethodByName("update");
-    Component.disableRemoteMethodByName("upsertWithWhere");
-    */
+
 };
