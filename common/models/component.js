f12db3d33c6d2613b4798ed0e794cad8fa515871
Mauricio Siller <mauriciosiller@gmail.com>
2018-11-04 15:32:07 -0600

    fix component model

--- common/models/component.js
+++ common/models/component.js
@@ -2,4 +2,20 @@
 
 module.exports = function(Component) {
 
+    Component.bulkInsert = function (data, cb) {
+        
+        Component.create(data)
+
+        cb(null, 'All data inserted!')
+    }
+
+    Component.updateArray = function(data, cb) {
+
+        data.forEach( elem => {
+            Component.upsert(elem);
+        });
+
+        cb(null, 'All data was updated!');
+    }
+
 };
