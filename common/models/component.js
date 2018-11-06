dd8e9bebca6beaae8649c3867aa8783ca155ff2d
Mauricio Siller <mauriciosiller@gmail.com>
2018-11-06 16:37:34 -0600

    update componenstack on component update

--- common/models/component.js
+++ common/models/component.js
@@ -18,4 +18,27 @@ module.exports = function(Component) {
         cb(null, 'All data was updated!');
     }
 
+    Component.afterRemote('replaceById', function(ctx, modelInstance, next) {
+
+        var app = Component.app;
+        var csResult = [];
+        var ComponentStack = app.models.ComponentStack;
+
+        ComponentStack.find({where: {componentId: ctx.args.id} }, function  (err, models) {
+
+            csResult = models;
+
+            csResult.forEach(function (item) {
+                item.name = ctx.args.data.name;
+            })
+
+            csResult.forEach( elem => {
+                ComponentStack.upsert(elem);
+            });
+
+        });
+
+        next();
+    });
+
 };
