453c2814de559fda192b019aeea718f6beb7ee8b
Mauricio Siller <mauriciosiller@gmail.com>
2018-11-06 17:27:14 -0600

    update batch

--- common/models/component-stack.js
+++ common/models/component-stack.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports = function(Componentstack) {
+    Componentstack.updateArray = function(data, cb) {
 
+        data.forEach( elem => {
+            Componentstack.upsert(elem);
+        });
+
+        cb(null, 'All data was updated!');
+    }
 };
