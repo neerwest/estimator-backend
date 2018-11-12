072c76ff721bb5835f490cfe0db456337d05c1f8
Mauricio Siller <mauriciosiller@gmail.com>
2018-11-12 17:19:26 -0600

    remove Phase childrens on delete

--- common/models/phase.js
+++ common/models/phase.js
@@ -2,4 +2,20 @@
 
 module.exports = function(Phase) {
 
+    Phase.afterRemote('deleteById', function(ctx, modelInstance, next) {
+        var phaseId = ctx.args.id;
+        var app = Phase.app;
+        var PhaseStack = app.models.PhaseStack;
+
+        PhaseStack.find({where: {phaseId: phaseId} }, function  (err, models) {
+
+            models.forEach( elem => {
+                PhaseStack.destroyById(elem.id, function(mod){ console.log(mod) });
+            });
+
+        });
+
+        next();
+    });
+
 };
