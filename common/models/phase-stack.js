7c43a32db2fed4c750ea8e90856f45076c48345e
Mauricio Siller <mauriciosiller@gmail.com>
2018-11-13 17:43:38 -0600

    addd update array for PhaseStack

--- common/models/phase-stack.js
+++ common/models/phase-stack.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports = function(Phasestack) {
+    Phasestack.updateArray = function(data, cb) {
 
+        data.forEach( elem => {
+            Phasestack.upsert(elem);
+        });
+
+        cb(null, 'All data was updated!');
+    }
 };
